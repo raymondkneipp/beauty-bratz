2ec1f2ad62cf45729b0400eb155b017622cca932
Raymond F. Kneipp III <raymond.f.kneipp@gmail.com>
2019-09-19 19:44:05 -0700

    Required input

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -15,13 +15,20 @@ const Contact = () => {
 					<form name="contact" method="post">
 						<input type="hidden" name="form-name" value="contact" />
 						<label htmlFor="name">Name</label>
-						<input type="text" name="name" id="name" placeholder="John Doe" />
+						<input
+							type="text"
+							name="name"
+							id="name"
+							placeholder="John Doe"
+							required
+						/>
 						<label htmlFor="phone">Phone</label>
 						<input
 							type="tel"
 							name="phone"
 							id="phone"
 							placeholder="(987) 654-3210"
+							required
 						/>
 						<label htmlFor="message">Message</label>
 						<textarea
@@ -29,6 +36,7 @@ const Contact = () => {
 							id="message"
 							placeholder="I would like to set up an appointment."
 							rows="5"
+							required
 						></textarea>
 						<button type="submit">
 							<span className="fas fa-paper-plane"></span> Send Message
